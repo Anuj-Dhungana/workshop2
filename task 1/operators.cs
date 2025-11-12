bc5332578b664cc88e6bc33fdc2ed58720826744
anujd <anujd@ANUJ>
2025-11-12 21:58:59 +0545

    Add project files.

---
+++ task 1/operators.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop2.task_1
+{
+    internal class operators
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+            //Console.WriteLine(a + b);
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator zero hudaina!");
+            }
+            return (double)a / b;
+        }
+
+        public void OddEvenFinder(int number)
+        {
+            string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ task 1/program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop2.task_1
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, from task 1");
+
+            var ops = new operators();
+
+            int a = 26;
+            int b = 4;
+            int number = 15;
+
+            Console.WriteLine($"Add: {ops.Add(a, b)}");
+            Console.WriteLine($"Subtract: {ops.Subtract(a, b)}");
+            Console.WriteLine($"Multiply: {ops.Multiply(a, b)}");
+            Console.WriteLine($"Divide: {ops.Divide(a, b)}");
+
+            ops.OddEvenFinder(number);
+        }
+    }
+}
