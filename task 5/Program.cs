5c6a4a4ab6807c75a85a6015ce616a8b1ae545cb
Anuj <anujdhungana6@gmail.com>
2025-11-12 22:58:28 +0545

    ädding the task 5

---
+++ task 5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop2.task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input any number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int resultIs = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                resultIs += i;
+            }
+            Console.WriteLine($"Sum from 1 to {n} is: {resultIs}");
+
+            int num = 1;
+            while (num <= 20)
+            {
+                // Skip multiples of 4
+                if (num % 4 == 0)
+                {
+                    num++; 
+                    continue;
+                }
+
+                // Break the loop when num is 15
+                if (num == 15)
+                {
+                    break;
+                }
+
+                Console.WriteLine(num);
+
+                num++;
+            }
+        }
+    }
+}
